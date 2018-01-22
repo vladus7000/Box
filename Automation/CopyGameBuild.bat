2113eb44b603ea32b3066c96055b8c3bd928e150
Vladyslav Reviakin <svladus7000@gmail.com>
2018-01-22 21:30:57 +0200

    fixed PrepareGameForLaunch.bat

--- Automation/CopyGameBuild.bat
+++ Automation/CopyGameBuild.bat
@@ -1,2 +1,3 @@
 echo "Copying game to the Game folder"
+copy ..\Artifacts\game\output\game.exe ..\Game
 exit /b 0
--- Automation/PrepareGameForLaunch.bat
+++ Automation/PrepareGameForLaunch.bat
@@ -1,1 +1,29 @@
+echo off
+echo "Preparing game for launch..."
+Setlocal EnableDelayedExpansion
+call CleanProjects.bat
+if %ERRORLEVEL% == 0 goto step2
+goto err
+:step2
+call GenProjects.bat
+if %ERRORLEVEL% == 0 goto step3
+goto err
+:step3
+call BuildGame.bat
+if %ERRORLEVEL% == 0 goto step4
+goto err
+:step4
+call CopyGameBuild.bat
+if %ERRORLEVEL% == 0 goto step5
+goto err
+:step5
+call BakeResources.bat
+if %ERRORLEVEL% == 0 goto step6
+goto err
+:step6
+mshta "about:<script>alert('Game should be able to run from <Root>/Game folder');close()</script>"
 exit /b 0
+
+err:
+mshta "about:<script>alert('Something went wrong');close()</script>"
+exit /b 1
